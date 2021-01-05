60265eacfd3bc923193c5f8847a84df74779cc9a
tmastalirsch <tmastalirsch@live.de>
2021-01-05 23:11:59 +0100

    add ingredient component

---
+++ src/components/Burger/Ingredient/IngredientComponent.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './Ingredients.module.css';
+
+const IngredientComponent = ({ type }) => {
+
+    const getIngredientDiv = (type) => {
+        let ingredient = null;
+        switch (type){
+            case ('bread-bottom'):
+                ingredient = <div className={classes.BreadBottom}></div>;
+                break;
+            case ('bread-top'):
+                ingredient = (
+                    <div className={classes.BreadTop}>
+                        <div className={classes.Seed1}></div>
+                        <div className={classes.Seed2}></div>
+                    </div>
+                );
+                break;
+            case ('meat'):
+                ingredient = <div className={classes.Meat}></div>;
+                break;
+            case ('cheese'):
+                ingredient = <div className={classes.Chesse}></div>;
+                break;
+            case ('bacon'):
+                ingredient = <div className={classes.Bacon}></div>;
+                break;
+            case ('salad'):
+                ingredient = <div className={classes.Salad}></div>;
+                break;
+            default:
+                ingredient = null;
+        }
+        return ingredient;
+    }
+
+    return getIngredientDiv(type);
+    
+}
+
+IngredientComponent.propTypes = {
+    type: PropTypes.string.isRequired
+}
+
+export default IngredientComponent;
